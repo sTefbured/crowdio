57af0e595f21ea3566ebfdac54987981b071715e
sTefbured <stef.for.ever2000@gmail.com>
2021-07-13 00:44:00 +0300

    Initial commit.

---
+++ src/main/java/com/stefbured/crowdio/CrowdioApplication.java
@@ -0,0 +1,13 @@
+package com.stefbured.crowdio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrowdioApplication.class, args);
+    }
+
+}
